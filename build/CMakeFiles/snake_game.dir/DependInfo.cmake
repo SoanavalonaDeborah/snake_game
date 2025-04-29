9c35e193123898cee441ebb93a050e67430323b2
SoanavalonaDeborah <deborahraza@protonmail.com>
2025-04-29 19:35:34 +0300

    first commit

---
+++ build/CMakeFiles/snake_game.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/deborah/Documents/Lesona/lesone_esti/L2/info_373/project/Snake-Game/main.cpp" "CMakeFiles/snake_game.dir/main.cpp.o" "gcc" "CMakeFiles/snake_game.dir/main.cpp.o.d"
+  "/home/deborah/Documents/Lesona/lesone_esti/L2/info_373/project/Snake-Game/src/food.cpp" "CMakeFiles/snake_game.dir/src/food.cpp.o" "gcc" "CMakeFiles/snake_game.dir/src/food.cpp.o.d"
+  "/home/deborah/Documents/Lesona/lesone_esti/L2/info_373/project/Snake-Game/src/game.cpp" "CMakeFiles/snake_game.dir/src/game.cpp.o" "gcc" "CMakeFiles/snake_game.dir/src/game.cpp.o.d"
+  "/home/deborah/Documents/Lesona/lesone_esti/L2/info_373/project/Snake-Game/src/snake.cpp" "CMakeFiles/snake_game.dir/src/snake.cpp.o" "gcc" "CMakeFiles/snake_game.dir/src/snake.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/snake_game.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_game.dir/main.cpp.o"
+  "CMakeFiles/snake_game.dir/main.cpp.o.d"
+  "CMakeFiles/snake_game.dir/src/food.cpp.o"
+  "CMakeFiles/snake_game.dir/src/food.cpp.o.d"
+  "CMakeFiles/snake_game.dir/src/game.cpp.o"
+  "CMakeFiles/snake_game.dir/src/game.cpp.o.d"
+  "CMakeFiles/snake_game.dir/src/snake.cpp.o"
+  "CMakeFiles/snake_game.dir/src/snake.cpp.o.d"
+  "snake_game"
+  "snake_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
